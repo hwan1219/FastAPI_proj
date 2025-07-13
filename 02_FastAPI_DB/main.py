c10ad81719baae158d8e01bba2f5235823516ead
hwan1219 <ljh961219@naver.com>
2025-07-14 00:02:39 +0900

    fastapi_proj

--- 02_FastAPI_DB/main.py
+++ 02_FastAPI_DB/main.py
@@ -2,8 +2,8 @@
 # FastAPI + SQLAlchemy + MySQL 예제
 
 # 1) 기본 라이브러리 임포트
-from typing import Optional # 선택적 필드를 위한 타입 힌트
-from fastapi import FastAPI, Request, Depends
+from typing import Optional, List # 선택적 필드를 위한 타입 힌트
+from fastapi import FastAPI, Request, Response, Depends, Path, HTTPException
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 from sqlalchemy.orm import Session # 실제 DB 세션 객체
@@ -27,9 +27,29 @@ class Memo(Base):
   content = Column(String(1000))
 
 # 5) Pydantic(검증) 스키마
+# 생성용
 class MemoCreate(BaseModel):
   title: str
   content: str
+
+# 수정용
+class MemoUpdate(BaseModel):
+  title: str
+  content: str
+  
+# 삭제용
+class MemoDelete(BaseModel):
+  title: str
+  content: str
+  
+# 출력용  
+class MemoResponse(BaseModel):
+  id: int
+  title: str
+  content: str
+  
+  class Config:
+    orm_mode = True
   
 # 6) DB 세션 의존성
 def get_db():
@@ -43,7 +63,7 @@ def get_db():
 # CRUD 라우트 정의
 
 # 1) 메모 생성
-@app.post("/memos")
+@app.post("/memos", response_model=MemoResponse, status_code=201)
 async def create_memo(
   memo: MemoCreate,
   db: Session = Depends(get_db)
@@ -52,8 +72,67 @@ async def create_memo(
   db.add(new_memo) # DB INSERT
   db.commit() # DB COMMIT
   db.refresh(new_memo) # DB id(PK) 포함 최신 상태 반영
+  
   return {
     "id": new_memo.id,
     "title": new_memo.title,
     "content": new_memo.content
   }
+  
+# 2) 메모 목록 조회
+@app.get("/memos", response_model=List[MemoResponse])
+async def get_memo(
+  db: Session = Depends(get_db)
+):
+  memos = db.query(Memo).all()
+  return memos
+
+# 3) 메모 수정
+@app.put("/memos/{memo_id}", response_model=MemoResponse, status_code=201)
+async def update_memo(
+  memo_id: int = ...,
+  memo: MemoUpdate = ...,
+  db: Session = Depends(get_db)
+):
+  # 기존 메모 조회
+  existing_memo = db.query(Memo).filter(Memo.id == memo_id).first()
+  
+  # 메모가 없으면 404 응답
+  if existing_memo is None:
+    raise HTTPException(
+      status_code=404,
+      detail="메모를 찾을 수 없습니다"
+    )
+  
+  # 값 덮어쓰기
+  existing_memo.title = memo.title
+  existing_memo.content = memo.content
+  
+  # 커밋 & 최신화
+  db.commit()
+  db.refresh(existing_memo)
+  
+  return {
+    "id": existing_memo.id,
+    "title": existing_memo.title,
+    "content": existing_memo.content
+  }
+  
+# 4) 메모 삭제
+@app.delete("/memos/{memo_id}", status_code=204)
+async def delete_memo(
+  memo_id: int = ...,
+  db: Session = Depends(get_db)
+):
+  memo = db.query(Memo).filter(Memo.id == memo_id).first()
+  
+  if memo is None:
+    raise HTTPException(
+      status_code=404,
+      detail="메모를 찾을 수 없습니다"
+    )
+  
+  db.delete(memo)
+  db.commit()
+  
+  return Response(status_code=204)
